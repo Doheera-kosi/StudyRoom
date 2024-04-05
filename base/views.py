6dfe668189c4ab19c4379c8dc9581571cdd73809
Doheera-kosi <kupourevans99@gmail.com>
2024-04-05 11:46:05 +0000

    Add home and room functions

--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# function base views
+
+
+def home(request):
+  return HttpResponse("Home page")
+
+
+def room(request):
+  return HttpResponse("Rooms Page")
