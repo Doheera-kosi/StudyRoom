15548cfa70243c3f44de0c9dd9b6a28710da6163
Doheera-kosi <kupourevans99@gmail.com>
2024-04-05 11:09:48 +0000

    Update settings.py with new created app, base

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Add new app below here
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
