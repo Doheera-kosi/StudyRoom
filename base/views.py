cc016dde2d274b96bb50884ca1ea6af827b726b0
Doheera-kosi <kupourevans99@gmail.com>
2024-04-05 20:58:15 +0000

    Add Room model

--- base/views.py
+++ base/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 from django.http import HttpResponse
 
 # function base views
 
-rooms = [
-  {'id': 1, 'name': 'Lets learn python!'},
-  {'id': 2, 'name': 'Build with Evans!'},
-  {'id': 3, 'name': 'Backend Engineer!'},
-]
+# rooms = [
+#   {'id': 1, 'name': 'Lets learn python!'},
+#   {'id': 2, 'name': 'Build with Evans!'},
+#   {'id': 3, 'name': 'Backend Engineer!'},
+# ]
 
 def home(request):
+  rooms = Room.objects.all()
   context = {'rooms': rooms}
   return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-  room = None
-  
-  for i in rooms:
-    if i ['id'] == int(pk):
-      room = i
-  
+  room = Room.objects.get(id=pk)
   context = {'room': room}
-  
   return render(request, 'base/room.html', context)
--- studyroom/settings.py
+++ studyroom/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     # Add new app below here
     "base.apps.BaseConfig",
 ]
