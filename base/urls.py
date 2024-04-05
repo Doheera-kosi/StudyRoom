3e78bc16ebd4d1df541664725d346d8872b81df8
Doheera-kosi <kupourevans99@gmail.com>
2024-04-05 11:47:57 +0000

    Add base app urls.py

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+# URLs PATHS FOR BASE APP
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('room/', views.room, name='room'),  
+]
