9dcbe38e5be136975ae8a72543ecd14decb4889b
Doheera-kosi <kupourevans99@gmail.com>
2024-04-08 23:31:53 +0000

    Add changes

---
+++ base/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Room
+
+# class RoomForm(ModelForm):
+#   class Meta:
+#     model = Room
+#     fields = '__all__'
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        exclude = ["created"]
