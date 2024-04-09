b795dbb497ff309cd40a3649a9b92fd7506e5b18
Doheera-kosi <kupourevans99@gmail.com>
2024-04-09 10:29:04 +0000

    Add recent activities

--- base/views.py
+++ base/views.py
@@ -55,7 +55,7 @@ def logoutUser(request):
 
 def registerUser(request):
     form = UserCreationForm()
-    
+
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -81,25 +81,34 @@ def home(request):
     topics = Topic.objects.all()
     room_count = rooms.count()
 
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all().order_by("-created")
     participants = room.participants.all()
-    
-    if request.method == 'POST':
-      message = Message.objects.create(
-        user=request.user,
-        room=room,
-        body=request.POST.get('body')
-      )
-      room.participants.add(request.user)
-      return redirect('room', pk=room.id)
-    
-    context = {"room": room, "room_messages": room_messages, 'participants': participants}
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user, room=room, body=request.POST.get("body")
+        )
+        room.participants.add(request.user)
+        return redirect("room", pk=room.id)
+
+    context = {
+        "room": room,
+        "room_messages": room_messages,
+        "participants": participants,
+    }
     return render(request, "base/room.html", context)
 
 
